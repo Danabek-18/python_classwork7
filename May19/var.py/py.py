dea135d866fd4f454930ee646f1171b216dc3237
Danabek-18 <134071374+Danabek-18@users.noreply.github.com>
2023-05-19 21:51:43 +0600

    1
    две задачи сделано

---
+++ May19/var.py/py.py
@@ -0,0 +1,15 @@
+#task 1
+user_input = input("Enter number")
+user_int1 =int(user_input)
+
+
+result = user_int + user_int /=2
+print("Result is", result)
+
+#task 2
+user_input = input("Enter number")
+user_int =int(user_input)
+
+
+result = user_int + user_int /=2
+print("Result is", result)
