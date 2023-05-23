e67e8e17f7e3048f00c46bdc3661a330fc90fe10
Danabek-18 <134071374+Danabek-18@users.noreply.github.com>
2023-05-23 21:45:50 +0600

    23 may

--- May19/var.py/py.py
+++ May19/var.py/py.py
@@ -3,13 +3,11 @@ user_input = input("Enter number")
 user_int1 =int(user_input)
 
 
-result = user_int + user_int /=2
+result = (user_int + user_int) /2
 print("Result is", result)
 
-#task 2
-user_input = input("Enter number")
-user_int =int(user_input)
+number=12345
+last_digit = number % 10
 
 
-result = user_int + user_int /=2
-print("Result is", result)
+print(last_digit)
